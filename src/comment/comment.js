e5d9c20d8d4090b30b4cedfad6b9b7c738bbb881
rchd <1228340520@qq.com>
2020-03-12 19:49:45 +0800

    This is test2

--- src/comment/comment.js
+++ src/comment/comment.js
@@ -11,7 +11,7 @@ class CommentList extends React.Component{
                         title="反馈"
                         subtitle="反馈列表"
             >
-                askdfjksdfj
+                iowepurqi
             </SideBar>
         );
     }
